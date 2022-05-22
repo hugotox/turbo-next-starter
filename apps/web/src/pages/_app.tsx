269c09b124915b945cc49ee29b21e08d2218bab1
Hugo Pineda <hugo@zumper.com>
2022-05-22 14:39:53 -0700

    added gmail auth

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { ApolloProvider } from '@apollo/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import { Global } from '@emotion/react'
 import { useApollo } from 'apollo-client'
+import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { theme, globalStyle } from 'theme'
@@ -11,16 +12,18 @@ import { wrapper } from '../lib/store'
 function App({ Component, pageProps }: AppProps) {
   const apolloClient = useApollo(pageProps)
   return (
-    <ApolloProvider client={apolloClient}>
-      <Head>
-        <meta content="width=device-width, initial-scale=1" name="viewport" />
-        <title>Turbo/Next/Chakra App</title>
-      </Head>
-      <ChakraProvider theme={theme}>
-        <Global styles={globalStyle} />
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </ApolloProvider>
+    <SessionProvider refetchInterval={0} session={pageProps.session}>
+      <ApolloProvider client={apolloClient}>
+        <Head>
+          <meta content="width=device-width, initial-scale=1" name="viewport" />
+          <title>Turbo/Next/Chakra App</title>
+        </Head>
+        <ChakraProvider theme={theme}>
+          <Global styles={globalStyle} />
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </ApolloProvider>
+    </SessionProvider>
   )
 }
 
---
+++ apps/web/src/pages/login.tsx
@@ -0,0 +1,39 @@
+/* eslint-disable @next/next/no-img-element */
+import { NextPageContext } from 'next'
+import { useSession, signIn, signOut, getSession } from 'next-auth/react'
+
+export default function Login() {
+  const { data: session } = useSession()
+  if (session) {
+    return (
+      <>
+        Signed in as {session?.user?.email} <br />
+        {session?.user?.image && (
+          <img
+            alt=""
+            height={80}
+            src={session.user.image}
+            style={{ borderRadius: '50%', overflow: 'hidden' }}
+            width={80}
+          />
+        )}
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    )
+  }
+  return (
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn('google')}>Sign in</button>
+    </>
+  )
+}
+
+// Export the `session` prop to use sessions with Server Side Rendering
+export async function getServerSideProps(context: NextPageContext) {
+  return {
+    props: {
+      session: await getSession(context),
+    },
+  }
+}
