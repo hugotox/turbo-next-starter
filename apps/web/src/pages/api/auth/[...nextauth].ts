d03d7170a67ad2c55ac188faa434d11ee92404af
Hugo Pineda <hugo@zumper.com>
2022-05-22 15:31:43 -0700

    add fb login

--- apps/web/src/pages/api/auth/[...nextauth].ts
+++ apps/web/src/pages/api/auth/[...nextauth].ts
@@ -1,13 +1,16 @@
 import NextAuth from 'next-auth'
+import FacebookProvider from 'next-auth/providers/facebook'
 import GoogleProvider from 'next-auth/providers/google'
 
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_ID ?? '',
+      clientSecret: process.env.FACEBOOK_SECRET ?? '',
     }),
-    // ...add more providers here
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID ?? '',
+      clientSecret: process.env.GOOGLE_SECRET ?? '',
+    }),
   ],
 })
