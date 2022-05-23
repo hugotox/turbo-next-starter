013b911659a0ad07cbe63134dff313e096d01edb
Hugo Pineda <hugo@zumper.com>
2022-05-22 17:08:42 -0700

    setup auth0

--- apps/web/src/pages/api/auth/[...nextauth].ts
+++ apps/web/src/pages/api/auth/[...nextauth].ts
@@ -1,16 +1,12 @@
 import NextAuth from 'next-auth'
-import FacebookProvider from 'next-auth/providers/facebook'
-import GoogleProvider from 'next-auth/providers/google'
+import Auth0Provider from 'next-auth/providers/auth0'
 
 export default NextAuth({
   providers: [
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_ID ?? '',
-      clientSecret: process.env.FACEBOOK_SECRET ?? '',
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID ?? '',
-      clientSecret: process.env.GOOGLE_SECRET ?? '',
+    Auth0Provider({
+      clientId: process.env.AUTH0_CLIENT_ID ?? '',
+      clientSecret: process.env.AUTH0_CLIENT_SECRET ?? '',
+      issuer: process.env.AUTH0_ISSUER,
     }),
   ],
 })
--- apps/web/src/pages/login.tsx
+++ apps/web/src/pages/login.tsx
@@ -24,7 +24,7 @@ export default function Login() {
   return (
     <>
       Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
+      <button onClick={() => signIn('auth0')}>Sign in</button>
     </>
   )
 }
