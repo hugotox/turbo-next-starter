327ea0c0feb2295cab01f512d48711a0f47e608f
Hugo Pineda <hugo@zumper.com>
2022-05-28 20:18:21 -0700

    upgrade testing lib & added redux provider to test render

--- apps/web/test/index.spec.tsx
+++ apps/web/test/index.spec.tsx
@@ -1,10 +1,11 @@
 import { renderWithProviders } from 'utils'
 
 import Index from '../src/pages/index'
+import { makeStore } from '../src/redux-store'
 
 describe('index', () => {
   it('renders', () => {
-    const { getByText } = renderWithProviders(<Index />)
+    const { getByText } = renderWithProviders(<Index />, { store: makeStore() })
     expect(getByText('Turbo Next Starter')).toBeInTheDocument()
   })
 })
--- packages/utils/src/testUtils/renderWithProviders.tsx
+++ packages/utils/src/testUtils/renderWithProviders.tsx
@@ -1,17 +1,34 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import { Store, configureStore } from '@reduxjs/toolkit'
 import { render, RenderOptions } from '@testing-library/react'
 import { ReactElement } from 'react'
+import { Provider } from 'react-redux'
 import { theme } from 'theme'
+
+const fallbackStore = configureStore({
+  reducer: (state: any) => state,
+})
 
 interface WithChildren {
   children?: ReactElement
 }
 
-const AllTheProviders = ({ children }: WithChildren) => {
-  return <ChakraProvider theme={theme}>{children}</ChakraProvider>
+interface Options extends RenderOptions {
+  store?: Store
 }
 
-const renderWithProviders = (ui: ReactElement, options?: RenderOptions) =>
-  render(ui, { wrapper: AllTheProviders, ...options })
+const WithReduxProvider = (store: Store) => {
+  return function WithChakraProvider({ children }: WithChildren) {
+    return (
+      <Provider store={store}>
+        <ChakraProvider theme={theme}>{children}</ChakraProvider>
+      </Provider>
+    )
+  }
+}
+
+const renderWithProviders = (ui: ReactElement, options?: Omit<Options, 'wrapper'>) => {
+  return render(ui, { wrapper: WithReduxProvider(options?.store ?? fallbackStore), ...options })
+}
 
 export { renderWithProviders }
